4ccc0004fb661ca4f79d68a8a4eb654913451a10
uruhao0819 <jay98011@yahoo.com.tw>
2021-12-30 21:42:20 +0800

    20211230

--- js/javascript.js
+++ js/javascript.js
@@ -74,3 +74,29 @@ function cal() {
 }
 cal();
 document.write("<br>" + AA)
+
+var a = 2; //全域變數
+function cal() {
+    // 連續宣告變數
+    // a = 5, B; 宣告變數給予值時，可以不用 var 定義 ; 宣告變數不予值時，需要用 var 定義才會成立
+    a = 5, b = 1; //全域變數
+    document.write("<br> a+b =" + (a+b));
+}
+cal();
+document.write("<br>" + a)
+
+// let 宣告的作用域 會因 { 區塊敘述 } 而分別定義為 全域或區域 的型態
+
+var a=5, b=0;
+let x=2, Y=0;
+{
+    var c= a+b;
+    let z= x+Y;
+}
+// 測試可用 控制台 呼叫
+console.log("c=",c);
+console.log("z=",z);
+
+// var 的全域或區域的作用，只存在 function ( 函數 ) 之內
+// { 區塊敘述 } if、eles、for、while
+// 判斷式 if {} eles {}
