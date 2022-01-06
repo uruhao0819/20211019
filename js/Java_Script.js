f2d6263e43b9c10711686fe4981c0fa204e0e81a
uruhao0819 <jay98011@yahoo.com.tw>
2022-01-06 21:46:26 +0800

    20220106

--- js/Java_Script.js
+++ js/Java_Script.js
@@ -12,5 +12,19 @@ for (let i = 0; i < Items.length; i++) {
 
     // 在哪個 html 標籤內，產生 li 清單物件
     let Div = document.getElementById('ProjectContent');
+    // 取得 html 標籤 ( 元素 ) 中添加的子元素
     Div.appendChild(liName);
 }
+
+// 變數 變更值
+let a = 3.14;
+console.log(a);
+a = 2;
+console.log(a);
+
+// 常數 無法變更值
+const pi = 3.14;
+
+console.log(pi);
+pi = 2;
+console.log(pi);
