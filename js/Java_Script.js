fadfc16fc32047f10c4b05dcf25cefee8c773a13
uruhao0819 <jay98011@yahoo.com.tw>
2022-01-06 21:33:26 +0800

    20220106

--- js/Java_Script.js
+++ js/Java_Script.js
@@ -1,1 +1,16 @@
 document.getElementById('ProjectContent').textContent="測試文字";
+
+let Items =['Item01','Item02','Item03'];
+
+for (let i = 0; i < Items.length; i++) {
+
+    // 在 html 內新增標籤 li
+    let liName = document.createElement('li');
+
+    // 陣列內的資料，置入 li 之內
+    liName.textContent = Items[i];
+
+    // 在哪個 html 標籤內，產生 li 清單物件
+    let Div = document.getElementById('ProjectContent');
+    Div.appendChild(liName);
+}
